8053b23cc853d2096e9b19144fdb709d19ab3a32
Raghav Kalani <raghav@example.com>
2026-03-05 00:49:25 +0530

    Fix: Use Create React App env variable syntax instead of Vite

--- bearly_trading_frontend/src/index.js
+++ bearly_trading_frontend/src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
